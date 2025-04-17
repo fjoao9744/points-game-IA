88061e1f69d694bdb9e9d507a640a412f415f39b
dyxm9 <jnoimek123@gmail.com>
2025-04-17 14:05:28 -0300

    Create main.lua

---
+++ game/main.lua
@@ -0,0 +1,57 @@
+player = {
+    pontos = 0,
+    x = 100,
+    y = 100
+}
+
+function love.load()
+    love.window.setTitle("game")
+
+    window_size_x, window_size_y = love.graphics.getDimensions()
+
+    x_obj, y_obj = math.random(0, window_size_x - 15), math.random(0, window_size_y - 15)
+
+end
+
+function love.update()
+    if love.keyboard.isDown("up") then
+        player.y = player.y - 5
+    end
+
+    if love.keyboard.isDown("down") then
+        player.y = player.y + 5
+    end
+
+    if love.keyboard.isDown("left") then
+        player.x = player.x - 5
+    end
+
+    if love.keyboard.isDown("right") then
+        player.x = player.x + 5
+    end
+
+
+    if checkCollision(player.x, player.y, 30, 30, x_obj, y_obj, 15, 15) then
+        player.pontos = player.pontos + 1
+        x_obj, y_obj = math.random(0, window_size_x - 15), math.random(0, window_size_y - 15)
+    end
+
+end
+
+function love.draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.rectangle("fill", x_obj, y_obj, 15, 15)
+    
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle("fill", player.x, player.y, 30, 30)
+
+    love.graphics.print("Pontos: " .. player.pontos)
+
+end
+
+function checkCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+    return x1 < x2 + w2 and
+           x2 < x1 + w1 and
+           y1 < y2 + h2 and
+           y2 < y1 + h1
+end
